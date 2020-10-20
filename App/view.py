36be0abeccc28b3ca91986291fa5449d05527c20
rafaf991 <42219559+rafaf991@users.noreply.github.com>
2020-10-20 18:50:11 -0500

    final

--- App/view.py
+++ App/view.py
@@ -84,7 +84,7 @@ while True:
 
     elif int(inputs[0]) == 4:
         initialDate = input("Rango Inicial (YYYY-MM-DD): ")
-        finalDate = input("Rango Inicial (YYYY-MM-DD): ")
+        finalDate = input("Rango Final (YYYY-MM-DD): ")
         total = controller.getAccidentsByRange(cont, initialDate, finalDate)
         print("\nTotal de accidentes en el rango de fechas: " + str(total))
 
